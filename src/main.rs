03367ad78e4fee15112abdb9da5b182e310f587a
Ryan James <realcoder2023@gmail.com>
2025-07-01 18:54:32 -0400

    added thread sleep function to apps lseep thread

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,5 @@
-use std::fs;
+use std::{fs, thread};
 use std::io::{stdin, Write};
-
 
 fn main() {
 
@@ -13,7 +12,13 @@ fn main() {
 
         stdin().read_line(&mut command).unwrap();
 
+        if command.len() < 1 {
+            panic!("Length of Command Not More: {}", command)
+        }
+
         if command.contains("Y") {
+
+            threads();
 
             get_file();
 
@@ -24,24 +29,41 @@ fn main() {
     }
 
 
+}
 
+fn threads() {
+
+    let x_sleep = 2000 / 2;
+
+    let m =  thread::sleep(x_sleep.abs());
+    return m;
+}
+
+fn is_empty(message: String) -> bool {
+
+    return message == ""
 }
 
 fn get_file() {
 
     let mut file = fs::File::create("index.html").unwrap();
 
-    fs::File::create( "main.css").unwrap();
-    fs::File::create("main.ts").unwrap();
+    if (is_empty(file)) {
+
+        fs::File::create( "main.css").unwrap();
+        fs::File::create("main.ts").unwrap();
 
 
-    file.write("<!DOCTYPE html>".as_ref()).expect("TODO: panic message");
-    file.write("<html lang='en'>".as_ref()).expect("TODO: panic message");
-    file.write("<title>".as_ref()).expect("TODO: panic message");
-    file.write("</title>".as_ref()).expect("TODO: panic message");
-    file.write("</html>".as_ref()).expect("TODO: panic message");
-    file.write("<body>".as_ref()).expect("TODO: panic message");
-    file.write("</body>".as_ref()).expect("TODO: panic message");
+        file.write("<!DOCTYPE html>".as_ref()).expect("TODO: panic message");
+        file.write("<html lang='en'>".as_ref()).expect("TODO: panic message");
+        file.write("<title>".as_ref()).expect("TODO: panic message");
+        file.write("</title>".as_ref()).expect("TODO: panic message");
+        file.write("</html>".as_ref()).expect("TODO: panic message");
+        file.write("<body>".as_ref()).expect("TODO: panic message");
+        file.write("</body>".as_ref()).expect("TODO: panic message");
+
+    }
+
 
 }
 
